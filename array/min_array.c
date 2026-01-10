dc0ab8df46730feacf62f69a159107662cb2bb70
nehasaroo <ne191206@gmail.com>
2026-01-10 20:53:52 +0530

    minimum of an  given array

---
+++ array/min_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int arr[100];
+    int min;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter array elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    min = arr[0];  
+    
+    for(i = 1; i < n; i++) {
+        if(arr[i] < min) {
+            min = arr[i];
+        }
+    }
+
+    printf("Minimum element = %d", min);
+
+    return 0;
+}
